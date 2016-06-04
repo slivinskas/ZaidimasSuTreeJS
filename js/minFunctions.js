0dccd6a98daeb5b982762bb1b11a0fc46f39a4cc
Slivinskas <slivinskasvytenis@gmail.com>
2016-06-04 13:01:17 +0300

    Skaldomas pagrindinis js failas

---
+++ js/minFunctions.js
@@ -0,0 +1,13 @@
+function onWindowResize() {
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
+        renderer.setSize( window.innerWidth, window.innerHeight );
+    }
+
+    function randomRange(min, max) {
+      return ~~(Math.random() * (max - min + 1)) + min
+    }
+
+    function randomHex(){
+        return Math.floor(Math.random()*16777215);/*.toString(16);*/
+    };
